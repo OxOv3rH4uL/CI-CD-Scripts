a1b83f6712d91dd24957df9bd743ba4bd1b3072c
OxOv3rH4uL <oterinaree@gmail.com>
2023-11-28 20:54:37 +0530

    vimeditpending

--- Nexus.sh
+++ Nexus.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-echo "Nexus"
+echo "##Nexus Installation###"
+sudo -s
+yum install wget -y
+yum install java-1.8.0-openjdk.x86_64 -y
+cd /opt
+wget -O latest-unix.tar.gz https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+tar -xvzf latest-unix.tar.gz
+sudo mv nexus-3* nexus
+mv sonatype-work nexusdata
+useradd --system nexus
+chown -R nexus:nexus /opt/nexus
+chown -R nexus:nexus /opt/nexusdata
